0b0e98a8b11ee4ad070b224af2f9583738ec0377
Tobias Slettemoen Kongsvik <tobias@kongsvik.dev>
2022-06-25 23:58:19 +0200

    fix

--- web/components/clock/Clock.tsx
+++ web/components/clock/Clock.tsx
@@ -1,19 +1,19 @@
 import { Box, Text, TextProps } from "@chakra-ui/react"
 import { useEffect, useState } from "react";
 
+const genTimeString = (locale?: string) => new Date().toLocaleTimeString([locale ?? "no"], { hour: '2-digit', minute: '2-digit' })
 const Clock = ({ locale, ...props }: { locale?: string} & TextProps) => {
-    const genTimeString = () => new Date().toLocaleTimeString([locale ?? "no"], { hour: '2-digit', minute: '2-digit' })
-    const [timeString, setTimeString] = useState(genTimeString())
+    const [timeString, setTimeString] = useState(genTimeString(locale))
 
     useEffect(() => {
         const start = new Date()
         let timer: NodeJS.Timer | null = null;
 
-        setTimeString(genTimeString());
+        setTimeString(genTimeString(locale));
         setTimeout(() => {
-            setTimeString(genTimeString());
+            setTimeString(genTimeString(locale));
             timer = setInterval(() => {
-                setTimeString(genTimeString());
+                setTimeString(genTimeString(locale));
             }, 60 * 1000);
         }, (60 - start.getSeconds()) * 1000)
 
@@ -21,7 +21,7 @@ const Clock = ({ locale, ...props }: { locale?: string} & TextProps) => {
             if (timer != null)
                 clearInterval(timer);
         }
-    }, [setTimeString, genTimeString])
+    }, [setTimeString, locale])
 
     return <Text {...props}>{timeString}</Text>
 }
