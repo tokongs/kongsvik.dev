a987136573177a60adc02472cbd4597aeffc36f1
Tobias Slettemoen Kongsvik <tobias@kongsvik.dev>
2022-06-25 21:33:12 +0200

    chakra

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,23 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Kongsvik</title>
         <meta name="description" content="Landing page for Tobias Slettemoen Kongsvik" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hello there!</h1>
+      <main className="">
+        <h1 className="">Hello there!</h1>
       </main>
-
-      <footer className={styles.footer}></footer>
-    </div>
+    </>
   )
 }
 
