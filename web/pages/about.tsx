d06079bee38ceb9cc588bd016a01d7563ede0b98
Tobias Slettemoen Kongsvik <tobias@kongsvik.dev>
2022-07-01 22:22:14 +0200

    typo

--- web/pages/about.tsx
+++ web/pages/about.tsx
@@ -5,10 +5,10 @@ const About: NextPage = () => {
   return (
       <Box maxW="2xl" m="auto">
         <Heading as="h1" size="xl" mb={4}>About the website</Heading>
-        <Text>This is a website based on NextJS serving content from a headless CMD called Sanity. 
+        <Text>This is a website based on NextJS serving content from a headless CMS called Sanity. 
           It is hosted on Vercel and the code can be found in 
-        </Text>
           <Link color="accent_pink.100" href="https://github.com/tokongs/kongsvik.dev" isExternal> this Github repository</Link>.
+        </Text>
       </Box>
   )
 }
