34c02b4e2669a0b994d2aa69eed93656dfd09540
Tobias Slettemoen Kongsvik <tobias@kongsvik.dev>
2022-07-11 21:37:50 +0200

    robots.txt

---
+++ web/app/routes/[robots.txt].tsx
@@ -0,0 +1,13 @@
+export const loader = () => {
+    const robotText = `
+    User-agent: *
+    Allow: /
+
+    `
+    return new Response(robotText,{
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+      }
+    });
+};
